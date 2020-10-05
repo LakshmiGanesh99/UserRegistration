1cfb087eef9cb612313a593c37dbeaa0fc1c963a
LakshmiGanesh99 <lakshmiganeshk99@gmail.com>
2020-10-05 08:47:38 +0530

    [LakshmiGaneshk99] add: First name validation feature

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,6 +1,16 @@
+import java.util.*;
+import java.util.regex.*;
+
 public class UserRegistration{
 	
 	public static void main(String[] args){
-			System.out.println("Welcome to user registration ");
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the First Name: ");
+		String firstName = sc.next();
+		while(!Pattern.matches("[A-Z]{1}[a-z]{2,}",firstName)){
+			System.out.println("Ivalid input. Enter the First Name: ");
+			firstName = sc.next();
+		}
 	}
 }
